b9a0d50681eac7bb5bc67fed7ce5f628dc63b291
lbj91 <lbj91@irontrain.co.kr>
2022-10-13 14:49:48 +0900

    noti test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
 import addItem from "./additem";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-dotenv.config();
+// dotenv.config();
 
-const key = process.env.NOTION_TOKEN ?? "";
-const db = process.env.NOTION_DATABASE_ID ?? "";
+// const key = process.env.NOTION_TOKEN ?? "";
+const key = core.getInput('GH_TOKEN');
 const notion = new Client({auth:key});
-const message = process.env.MESSAGE ?? "";
-const author = process.env.AUTHOR ?? "";
-const repository = process.env.REPOSITORY ?? "";
-const date = process.env.DATE ?? "";
+// const db = process.env.NOTION_DATABASE_ID ?? "";
+// const message = process.env.MESSAGE ?? "";
+// const author = process.env.AUTHOR ?? "";
+// const repository = process.env.REPOSITORY ?? "";
+// const date = process.env.DATE ?? "";
+const db = core.getInput('NOTION_DATABASE_ID');
+const message = core.getInput('MESSAGE');
+const author = core.getInput('AUTHOR');
+const repository = core.getInput('REPOSITORY');
+const date = core.getInput('DATE');
 
 async function main(client:Client, database_id: string) {
 
